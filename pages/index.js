2e0ae6750b42b95268dacd9eadc16c2ba7ccf8c8
benjitrosch <benjitrosch@gmail.com>
2021-02-03 12:11:41 -0500

    set up custom reactflow node

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,75 @@
 import Head from 'next/head'
 import ReactFlow, { Handle }  from 'react-flow-renderer';
 
+import Node from '../components/Node.js';
+
+const onNodeDragStop = (event, node) => console.log('drag stop', node);
+const onElementClick = (event, element) => console.log('click', element);
+
+const nodeTypes = {
+  selectorNode: Node,
+};
+
+const data = {
+	tables: [
+		{ 
+			name: 'people',
+			columns: [
+				{
+					name: '_id',
+					dataType: 'Int',
+					required: true 
+				},
+				{
+					name: 'name',
+					dataType: 'string',
+					required: true 
+				},
+				{
+					name: 'age',
+					dataType: 'Int',
+					required: true
+				},
+				{
+					name: 'homeworld_id',
+					dataType: 'int',
+					required: false
+				}
+			] 
+		}
+  ],
+	connections: [ 
+		{ 
+			origin: 'people',
+			originKey:  'species_id',
+			destination: 'species',
+			destinationKey: '_id',
+		}
+	]
+}
+
 const elements = [
   {
     id: "1",
-    type: "input",
-    data: { label: "Master Node" },
+    type: "selectorNode",
+    data: {
+      label: (
+        <div>
+          <Node tablename={data.tables[0].name} columns={data.tables[0].columns} />
+        </div>
+        ),
+      },
     position: { x: 50, y: 50 }
   },
   { id: "2", data: { label: "Node 2" }, position: { x: 100, y: 100 } },
   { id: "3", data: { label: "Node 3" }, position: { x: 250, y: 150 } },
-  { id: "4", data: { label: "Node 4" }, position: { x: 500, y: 200 } },
-  { id: "5", data: { label: "Node 5" }, position: { x: 750, y: 250 } },
-  {
-    id: "6",
-    data: { label: "Node 6" },
-    position: { x: 800, y: 300 },
-    type: "output"
-  },
-  { id: "e1-2", source: "3", target: "2", type: "straight" },
-  { id: "e1-3", source: "1", target: "3", type: "default" },
-  { id: "e1-4", source: "1", target: "4", type: "default" },
-  { id: "e1-5", source: "5", target: "2", type: "step", animated: true },
-  { id: "e1-6", source: "1", target: "6", type: "step" }
+  { id: "e1a-2", source: "1", target: "2", sourceHandle: "a", type: "smoothstep", animated: true },
+  { id: "e1b-3", source: "1", target: "3", sourceHandle: "b", type: "smoothstep", animated: true },
 ];
 
-const graphStyles = { width: "100%", height: "500px" };
+const graphStyles = { width: "100%", height: "100%px" };
 
-const BasicGraph = () => <ReactFlow elements={elements} style={graphStyles} />;
+const BasicGraph = () => <ReactFlow nodeTypes={nodeTypes} elements={elements} style={graphStyles} onElementClick={onElementClick} onNodeDragStop={onNodeDragStop} />;
 
 export default function Home() {
   return (
