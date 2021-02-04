69ae5f6109770e6e2c68b85d72dfb06c683ae2b0
Eric Peng <tzerpeng@gmail.com>
2021-02-03 19:27:13 -0500

    create error handling for bad URI

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,15 @@
 const { Pool } = require('pg');
 
 module.exports = {
-  connectToDB: (URI) => {
-    return new Pool({ connectionString: URI });
+  connectToDB: async (URI) => {
+    return new Promise((resolve) => {
+      const pool = new Pool({ connectionString: URI });
+      // test if db connection works
+      pool.query("SELECT 1", (err, data) => {
+        if (err) return resolve(new Error(err));
+        return resolve(pool);
+      }); 
+    });
   },
   asyncQuery: async (pool, query, params) => {
     return new Promise((resolve) => {
@@ -14,6 +21,6 @@ module.exports = {
   },
   errorHandler: (res, err) => {
     console.log(`Error in middleware function: ${err}`);
-    res.status(400).JSON('Error in middleware function- check server logs for more information');
+    res.status(400).json('Error in middleware function- check server logs for more information');
   }
 }
