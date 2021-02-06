70c9d89e6b5c2e78d3e051da7284ec8b55133f59
dasnyder3 <dasnyder3@gmail.com>
2021-02-06 12:14:00 -0500

    type defs partially completed for each table

---
+++ converters/TypeDefs.js
@@ -0,0 +1,29 @@
+const { plural, singular } = require('pluralize');
+const { convertDataType, capitalizeFirstLetter } = require('./helpers.js');
+
+//turn table database into GraphQL format schema
+function tableToType(table) {
+    let upperCaseL = capitalizeFirstLetter(table.name);
+    let type = `type ${singular(upperCaseL)} { \n`;
+
+    table.columns.forEach((col) => {
+      type += ' ' + mapColumn(col) + '\n';
+    })
+    type += '}'
+    return type;
+}
+
+//turn a single column object into a line in the GraphQL object type
+function mapColumn(column) {
+  let str = `${column.name}: ${convertDataType(column.dataType)}`;
+  if (column.required) str += '!';
+  return str;
+}
+
+function generateAllTypes(tables) {
+  let allTypes = ``;
+  tables.forEach((table) => {
+    allTypes += tableToType(table) + '\n\n';
+  });
+  return allTypes;
+}
