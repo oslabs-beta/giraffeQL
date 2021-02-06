c055a6a168fb95ac2952c7119a92cab8974c7ee0
benjitrosch <benjitrosch@gmail.com>
2021-02-06 11:36:26 -0500

    fixed crash on new select in edit mode

--- components/Inspector.js
+++ components/Inspector.js
@@ -13,33 +13,71 @@ const Inspector = (data) =>{
 
     const [expand, showTable] = useState(true);
     const [editable, toggleEdit] = useState(false);
+    const [tableName, setTableName] = useState(props.tablename);
 
     //We make an exact copy of our currently activeNode in our state
     const [activeNode, updateNode] = useState(data.data);
 
+    useEffect(() => {
+
+        const onKeyDown = ({key}) => {
+            if (key === "Enter" && editable)
+                return (toggleEdit(false), data.nodeValueChange(activeNode));
+        }
+
+        document.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', onKeyDown);
+        }
+
+    });
+
     //When the data (props) as activeNode being sent to the inspector change, we update the activeNode state
     useEffect(()=>{
-        updateNode(data.data)
+
+        updateNode(data.data);
+        setTableName(data.data.data.label.props.children.props.tablename);
+        
     }, [data]);
 
-    // useEffect(() => {
-    //     console.log(activeNode);
-    // }, [activeNode]);
+    useEffect(() => {
+
+        if (!store.selectedElements)
+            return;
+
+        toggleEdit(false);
+
+    }, [store.selectedElements]);
+
+    useEffect(() => {
+
+        const newNode = JSON.parse(JSON.stringify(activeNode))
+        newNode.data.label.props.children.props.tablename = tableName;
+        
+        updateNode(newNode);
+
+    }, [tableName]);
 
     const colors=['#ff6b6b', '#f9844aff', '#fee440', '#02c39a', '#4361ee', '#9b5de5', '#f15bb5'];
 
     return (
         <div id='inspector' style={{transform: `${expand ? '' : 'translateX(-360px)' }`, position: `${expand ? 'relative' : 'fixed'}`}} >
 
-            <button id='editbtn' onClick={()=>showTable(!expand)} style={{transform: `${expand ? '' : 'translateX(288px)' }`}} >{expand ? '<' : '>'}</button>
-
-            <button id='submitbtn' onClick={()=>data.nodeValueChange(activeNode)} >Submit</button>
+            <button id='inspectorbtn' onClick={()=>showTable(!expand)} style={{transform: `${expand ? '' : 'translateX(288px)' }`}} >{expand ? '<' : '>'}</button>
 
             <div id='sidebar' >
 
-                <div onClick={()=>toggleEdit(!editable)} ><Pencil /></div>
-                <div id='tablename' style={{borderLeft: `8px solid ${colors[props.nodeid % colors.length]}`}} >{props.tablename}</div>
+                {/* Edit Button */}
+                <div onClick={()=>{if(editable)data.nodeValueChange(activeNode); toggleEdit(!editable)}} ><Pencil /></div>
 
+                {/* Tablename */}
+                <div id='tablename' style={{borderLeft: `8px solid ${colors[props.nodeid % colors.length]}`, backgroundColor: `${editable ? '#c0dbfd' : 'white'}`}} >
+                    <input className='tablenameinput' value={tableName} type='text' disabled={editable ? '' : 'disabled'} onChange={(e) => setTableName(e.target.value)} style={{color: `${editable ? '#4754bd' : 'black'}`, backgroundColor: `${editable ? '#c0dbfd' : 'white'}`}} />
+
+                </div>
+
+                {/* Columns */}
                 {props.columns.map((column, i) => <ColumnInspector name={column.name} index={i} id={`${column.name}#${i}`} key={`${column.name}#${i}`} dataType={column.dataType} editable={editable} activeNode={activeNode} updateNode={updateNode} />)}
 
             </div>
@@ -69,7 +107,21 @@ const Inspector = (data) =>{
                     box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.06);
                 }
 
-                #editbtn{
+                .tablenameinput{
+                    border: none;
+                    outline: none;
+                    font-size: 24px;
+                    text-align: left;
+                    color: black;
+                    width: 100%;
+                    
+                    &:disabled{
+                        background-color: white;
+                        color: black;
+                    }
+                }
+
+                #inspectorbtn{
                     font-size: 24px;
                     font-family: 'Inter', sans-serif;
                     position: fixed;
