fde054079af270e30bb4f5ac1cb4ce80720bdbb0
dasnyder3 <dasnyder3@gmail.com>
2021-02-07 15:54:37 -0500

    adding logic to include join tables in type def generation

--- converters/TypeDefs.js
+++ converters/TypeDefs.js
@@ -1,33 +1,66 @@
 const { plural, singular } = require('pluralize');
-const { convertDataType, capitalizeFirstLetter } = require('./helpers.js');
+const { 
+  convertDataType, 
+  capitalizeFirstLetter, 
+  findJoinTables,
+  mapJoinConnections,
+  sortTables
+} = require('./helpers.js');
 
-// const exampleData = require('../exampledata.json');
+const exampleData = require('../exampledata.json');
 
 //turn table database into GraphQL format schema
-function tableToType(table) {
+function tableToType(table, joinConnections) {
     let upperCaseL = capitalizeFirstLetter(table.name);
     let type = `type ${singular(upperCaseL)} { \n`;
 
     table.columns.forEach((col) => {
       type += ' ' + mapColumn(col) + '\n';
     })
+
+    if (table.connections.length) {
+      table.connections.forEach((conn) => {
+        type += ' ' + mapConnection(conn.destinationTable) + '\n';
+      })
+    }
+
+    if (joinConnections) {
+      joinConnections.forEach((jc) => {
+        type += ` ${mapConnection(jc)}\n`;
+      })
+    }
+
     type += '}'
     return type;
 }
 
-//turn a single column object into a line in the GraphQL object type
+//turn a single column object into a line in the GraphQL object definition
 function mapColumn(column) {
   let str = `${column.name}: ${convertDataType(column.dataType)}`;
   if (column.required) str += '!';
   return str;
 }
 
+// turns a referenced tables into a line in the GraphQL object definition
+function mapConnection(connection) {
+  const connectedObj = capitalizeFirstLetter(singular(connection));
+  return `${connection}: [${connectedObj}]`;
+}
+
+// highest level function that takes a list of tables and their connections
+// returns the graphQL TypeDefs 
 function generateAllTypes(tables) {
-  let allTypes = ``;
-  tables.forEach((table) => {
-    allTypes += tableToType(table) + '\n\n';
+  let allTypes = `TypeDefs = \` \n`;
+  const [baseTables, joinTables] = sortTables(tables);
+  const joinTables = findJoinTables(tables);
+  const tablesToType = tables.filter((table) => {
+    return !Object.keys(joinTables).includes(table.name)
   });
-  return allTypes;
+  const allJoinConnections = mapJoinConnections(joinTables);
+  tablesToType.forEach((table) => {
+    allTypes += tableToType(table, allJoinConnections[table.name]) + '\n\n';
+  });
+  return allTypes + `\``;
 }
 
 module.exports = generateAllTypes;
--- converters/helpers.js
+++ converters/helpers.js
@@ -17,6 +17,45 @@ module.exports = {
 
   capitalizeFirstLetter: (string) => {
     return string[0].toUpperCase() + string.slice(1);
+  },
+
+  // function to identify which tables are join tables 
+  // assumes that a join table will be made up entirely of connections 
+    // aside from the primary key of the table
+  findJoinTables: (tables) => {
+    return tables.reduce((joinTables, table) => {
+      if (table.columns.length - table.connections.length <= 1) {
+        joinTables[table.name] = table.connections.map((conn) => {
+          return conn.destinationTable;
+        })
+      }
+      return joinTables;
+    }, {})
+  },
+
+  sortTables: (tables) => {
+    const baseTables = [];
+    const joinTables = [];
+    tables.forEach((table) => {
+      if (table.columns.length - tables.connections.length <= 1) {
+        joinTables.push(table);
+      } else {
+        baseTables.push(table);
+      }
+    })
+    return [baseTables, joinTables];
+  }
+
+  mapJoinConnections: (joinTables) => {
+    return Object.keys(joinTables).reduce((connections, currTable) => {
+      joinTables[currTable].forEach((ele, idx) => {
+        if (!connections[ele]) connections[ele] = [];
+        const connectedTables = joinTables[currTable].slice()
+        connectedTables.splice(idx,1);
+        connections[ele].push(...connectedTables);
+      })
+      return connections;
+    }, {})
   }
 
 }
