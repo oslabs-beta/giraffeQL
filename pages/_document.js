1881ff7c72aa80d08e090974edb609f583428269
dasnyder3 <dasnyder3@gmail.com>
2021-02-16 19:37:35 -0500

    removing console.log

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,7 @@ import { setCookie } from 'nookies';
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
-        console.log('ctx.query: ', ctx.query)
         if (ctx.query.code) {
-            // console.log('about to set cookie');
             setCookie(ctx, 'authorization', ctx.query.code, {
                 maxAge: 30 * 24 * 60 * 60,
                 path: '/',
