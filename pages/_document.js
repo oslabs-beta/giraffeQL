2c45e535cb1d73ef1beacc9eaf5e737feed2a9d4
dasnyder3 <dasnyder3@gmail.com>
2021-02-19 12:27:54 -0500

    updated cookie setting

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ import { setCookie } from 'nookies';
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
-        if (ctx.query.code) {
-            setCookie(ctx, 'authorization', ctx.query.code, {
+        if (ctx.query.token) {
+            setCookie(ctx, 'authorization', ctx.query.token, {
                 maxAge: 30 * 24 * 60 * 60,
                 path: '/',
             });
