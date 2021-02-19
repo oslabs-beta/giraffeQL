82b9fae36a71eac6dd841e4a9ff568b46913a9d2
benjitrosch <benjitrosch@gmail.com>
2021-02-18 23:19:47 -0500

    testing new endpoints

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Home = (props) => {
 
               <GiraffeQL />
 
-              <Link href={process.env.NODE_ENV === 'development' ? `http://localhost:3001/auth/github` : `https://giraffeql.io/auth/github`}>
+              <Link href={process.env.NODE_ENV === 'development' ? `http://localhost:3001/auth/github` : `https://giraffeql-api.herokuapp.com/auth/github`}>
                 <button><span>Sign in With GitHub<GitHub /></span></button>
               </Link>
 
@@ -268,7 +268,7 @@ const Home = (props) => {
 
 async function getUser(authorization) {
 
-  const fetchURL = process.env.NODE_ENV === 'development' ? `http://localhost:3001` : `https://giraffeql.io`;
+  const fetchURL = process.env.NODE_ENV === 'development' ? `http://localhost:3001` : `https://giraffeql-api.herokuapp.com`;
   const res = await fetch(`${fetchURL}/user`, { headers: { authorization } })
 
   if (res.status === 200) return { authorization, user: res.data }
