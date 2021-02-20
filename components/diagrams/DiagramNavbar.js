8b721e5ea46d630054be0660a50f7cd849f48948
Anson Avellar <anson@ansonavellar.com>
2021-02-20 16:56:19 -0500

    fixed styling of diagrams page

--- components/diagrams/DiagramNavbar.js
+++ components/diagrams/DiagramNavbar.js
@@ -20,48 +20,40 @@ const DiagramNavbar = () => {
       <style jsx>{`
 
           @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700;900&display=swap');
-
           *{
             font-family: 'Inter', sans-serif;
             font-weight: 300;
             font-size: 16px;
             transition: all .3s;
-          }
+        }
 
-          .navbar {
+        .homebtn{
+            display: flex;
+            width: 12.5%;
+            &:hover{
+                background-color: #5f81e7;
+                cursor: pointer;
+            }
+        }
+
+        .navbar {
             display: flex;
             align-items: center;
             justify-content: space-between;
             overflow: hidden;
-            // background-color: #5661b3;
+            background-color: #5661b3;
             position: fixed;
             top: 0;
             width: 100%;
             height: 39px;
             padding: 8px;
             z-index: 9999999999999999999999999;
-          }
+        }
 
-          h1 {
-            color: #edf2f7;
-            border-radius: 8px;
-            padding: 8px;
-          }
-
-          h2 {
-            color: #b2b7ff;
-            font-weight: 500;
-            font-size: 24px;
-          }
-
-          .homebtn{
-            display: flex;
-            
-            &:hover{
-              background-color: #5f81e7;
-              cursor: pointer;
-            }
-          }
+        #profile{
+          margin-left: 75%;
+          width: 25%;
+        }
 
       `}</style>
 
