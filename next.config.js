594ddd32147deca16733c2ba3ed15bdce261e11a
CodeFuzzCC <60477360+CodeFuzzCC@users.noreply.github.com>
2021-02-23 20:10:44 -0500

    allow test to ignore .next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  },
   async rewrites() {
     return [
       {
